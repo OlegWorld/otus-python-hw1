9c8700ef9b04abf8aa4d1923ed9f19e0d7491163
Mironov, Oleg <oleg.mironov@krontech.ru>
2020-06-06 19:39:38 +0300

    Review update

--- log_analyzer.py
+++ log_analyzer.py
@@ -7,6 +7,7 @@ import sys
 import json
 import logging
 from datetime import date
+from collections import namedtuple
 
 # log_format ui_short '$remote_addr  $remote_user $http_x_real_ip [$time_local] "$request" '
 #                     '$status $body_bytes_sent "$http_referer" '
@@ -22,26 +23,30 @@ config = {
 }
 
 
-def read_argv(args):
-    if 2 <= len(args) <= 3:
-        if args[1] == '--config':
-            return read_config(args[2] if len(args) == 3 else 'config.json')
-
-        else:
-            print('unknown command line argument')
-            return False
-
-    elif len(args) > 3:
-        print('too many command line arguments')
-        return False
+DateLog = namedtuple('DateLog', ('date', 'logfile'))
+LogMetrics = namedtuple('LogMetrics', ('url', 'time'))
 
 
-def read_config(config_file):
+def read_argv(global_config):
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Log analyzer')
+    parser.add_argument(
+        '--config',
+        type=str,
+        default='config.json',
+        help='provide configuration file'
+    )
+
+    return read_config(parser.parse_args().config, global_config)
+
+
+def read_config(config_file, global_config):
     try:
         with open(config_file, 'r') as c_file:
             try:
                 new_config = json.load(c_file)
-                config.update(new_config)
+                global_config.update(new_config)
                 return True
 
             except json.decoder.JSONDecodeError:
@@ -53,22 +58,47 @@ def read_config(config_file):
         return False
 
 
-def read_paths(log_path, report_path):
-    log_files = list(filter(lambda filename: filename.startswith('nginx-access-ui.log-'), os.listdir(log_path)))
-    dates = [date(*[int(num) for num in re.match('.*.log-(\d{4})(\d{2})(\d{2}).*', log).groups()]) for log in log_files]
-    last_date, last_date_log = max(zip(dates, log_files))
+def configurate_logger(logger_file_name):
+    logger_config = {
+        'filemode': 'w',
+        'level': logging.INFO,
+        'format': '[%(asctime)s] %(levelname).1s %(message)s',
+        'datefmt': '%Y.%m.%d %H:%M:%S'
+    }
 
-    if check_report(report_path, last_date):
-        logging.info('report file is up-to-date')
-        return None
+    if logger_file_name:
+        logger_config['filename'] = logger_file_name
     else:
-        logging.info('creating report for {} file'.format(last_date_log))
-        return last_date_log, last_date
+        logger_config['stream'] = sys.stdout
+
+    logging.basicConfig(**logger_config)
 
 
-def check_report(path, report_date):
-    report_files = os.listdir(path)
-    return 'report-' + report_date.strftime('%Y.%m.%d') + '.html' in report_files
+def read_paths(log_path):
+    file_begin = 'nginx-access-ui.log-'
+    date_pattern = r'.*.log-(\d{4})(\d{2})(\d{2}).*'
+    last_date, last_log_file = max(
+        [(date(*[int(num) for num in re.match(date_pattern, log).groups()]), log)
+         for log in filter(lambda filename: filename.startswith(file_begin), os.listdir(log_path))]
+    )
+
+    return DateLog(last_date, last_log_file)
+
+
+def check_report(report_path, report_date):
+    file_to_seek = os.path.join(report_path, 'report-' + report_date.strftime('%Y.%m.%d') + '.html')
+
+    if os.path.exists(report_path):
+        if os.path.exists(file_to_seek):
+            logging.info('report file {} is up-to-date'.format(file_to_seek))
+            return None
+
+    else:
+        logging.info('creating a non-existent report directory: {}'.format(report_path))
+        os.mkdir(report_path)
+
+    logging.info('creating report for {} file'.format(file_to_seek))
+    return file_to_seek
 
 
 def analyze_log_file(filename):
@@ -79,13 +109,15 @@ def analyze_log_file(filename):
         'encoding': 'utf-8'
     }
 
+    opener = gzip.open if filename.endswith('.gz') else open
+
     try:
-        with gzip.open(filename, **open_params) if filename.endswith('.gz') else open(filename, **open_params) as file:
+        with opener(filename, **open_params) as file:
             for line in file.readlines():
                 line, request_time_str = line.rsplit(sep=' ', maxsplit=1)
 
                 try:
-                    url = re.search('\"[A-Z]*\s*([^\"\s]*)\s[^\"]*\"', line).group(1)
+                    url = re.search(r'\"[A-Z]*\s*([^\"\s]*)\s[^\"]*\"', line).group(1)
                 except AttributeError:
                     logging.info('url not found in: {}'.format(line))
                     yield ()
@@ -96,7 +128,8 @@ def analyze_log_file(filename):
                     logging.info('invalid time: {}'.format(request_time_str))
                     yield ()
 
-                yield url, request_time
+                yield LogMetrics(url, request_time)
+
     except IOError:
         logging.error("error opening the analysable file {}".format(filename))
 
@@ -117,11 +150,9 @@ class Statistics:
         self.requests = {}
 
     def update(self, metrics):
-        url, time = metrics
-        if url in self.requests:
-            self.requests[url].append(time)
-        else:
-            self.requests[url] = [time]
+        time_list = self.requests.setdefault(metrics.url, [metrics.time])
+        if time_list != [metrics.time]:
+            time_list.append(metrics.time)
 
     def get_report(self, report_size, sort_by='time_sum'):
         if sort_by not in ['url', 'count', 'count_perc', 'time_sum', 'time_perc', 'time_avg', 'time_max', 'time_med']:
@@ -153,18 +184,17 @@ class Statistics:
         return full_report[: report_size]
 
 
-def write_report(report, report_date, report_template='report.html'):
+def write_report(report, report_file, report_template='report.html'):
     from string import Template
 
-    report_file_name = 'report-' + report_date.strftime('%Y.%m.%d') + '.html'
-    logging.info('generating report file {}'.format(report_file_name))
+    logging.info('generating report file {}'.format(report_file))
 
     try:
         with open(report_template, mode='rt', encoding='utf-8') as template_file:
             template = Template(template_file.read())
             result = template.safe_substitute(table_json=json.dumps(report))
 
-            with open(report_file_name, mode='wt', encoding='utf-8') as rep_file:
+            with open(report_file, mode='wt', encoding='utf-8') as rep_file:
                 rep_file.write(result)
 
     except IOError:
@@ -172,44 +202,36 @@ def write_report(report, report_date, report_template='report.html'):
 
 
 def main():
-    args = sys.argv
-    if not read_argv(args):
+    if not read_argv(config):
         return
 
-    logger_config = {
-        'filemode': 'w',
-        'level': logging.INFO,
-        'format': '[%(asctime)s] %(levelname).1s %(message)s',
-        'datefmt': '%Y.%m.%d %H:%M:%S'
-    }
-    if "LOG_FILE" in config:
-        logger_config['filename'] = config["LOG_FILE"]
-    else:
-        logger_config['stream'] = sys.stdout
-
-    logging.basicConfig(**logger_config)
+    configurate_logger(config.get('LOG_FILE'))
 
     try:
-        log_info = read_paths(config["LOG_DIR"], config["REPORT_DIR"])
-        if log_info:
-            log_file, log_date = log_info
-            statistics = Statistics()
-            total, errors = 0, 0
-            for metrics in analyze_log_file(log_file):
-                total += 1
-                if metrics:
-                    statistics.update(metrics)
-                else:
-                    errors += 1
+        log_info = read_paths(config["LOG_DIR"])
 
-            if errors / total > 0.5:
-                logging.error('too many parsing errors. Aborting')
-                return
+        report_file = check_report(config["REPORT_DIR"], log_info.date)
 
-            report = statistics.get_report(config["REPORT_SIZE"], config["REPORT_SORT"])
+        if not report_file:
+            return
 
-            if report:
-                write_report(report, log_date, config['REPORT_TEMPLATE'])
+        statistics = Statistics()
+        total, errors = 0, 0
+        for metrics in analyze_log_file(log_info.logfile):
+            total += 1
+            if metrics:
+                statistics.update(metrics)
+            else:
+                errors += 1
+
+        if errors / total > 0.5:
+            logging.error('too many parsing errors. Aborting')
+            return
+
+        report = statistics.get_report(config["REPORT_SIZE"], config["REPORT_SORT"])
+
+        if report:
+            write_report(report, report_file, config['REPORT_TEMPLATE'])
 
     except BaseException:
         logging.exception("Unexpected exception occured with traceback:")
